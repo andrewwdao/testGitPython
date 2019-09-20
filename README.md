477abb5fc8b354e54bfa2523fdc6c018f09521d1
Minh An Dao <minhan7497@gmail.com>
2019-09-20 17:59:49 +0700

    add an other method to solve the problem

---
+++ README.md
@@ -0,0 +1,6 @@
+SAMPLE GIT REPO FOR SAVING SOME PYTHON SCRIPT
+THIS WILL BE A SUBMODULE OF THE TESTGIT REPO
+
+The purpose of this repo is only for learning Git, along with repo testGit
+
+Last update: 09/2019
--- week1/mergeOrderedList.py
+++ week1/mergeOrderedList.py
@@ -9,8 +9,33 @@ testList2 = [2,5,7,8,9,10,23]
 print("The 1st original list is: " + str(testList1))
 print("The 2nd original list is: " + str(testList2))
 
+#--------------------------------------------------------------------------
+#FIRST METHOD:
+# using naive method  
+# to combine two sorted lists 
+size_1 = len(testList1) 
+size_2 = len(testList2) 
+  
+res = [] 
+i, j = 0, 0
+  
+while i < size_1 and j < size_2: 
+    if testList1[i] < testList2[j]: 
+      res.append(testList1[i]) 
+      i += 1
+  
+    else: 
+      res.append(testList2[j]) 
+      j += 1
+  
+res = res + testList1[i:] + testList2[j:] 
+#--------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------
+#SECOND METHOD:
 #using heapq.merge() to combine two sorted lists
 res = list(merge(testList1,testList2))
+#--------------------------------------------------------------------------
 
 #printing result
 print("The combined sorted list is: "+str(res))

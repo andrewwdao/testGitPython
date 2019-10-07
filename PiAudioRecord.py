c264ccfe1b23742c3fa1107c156c482c88ac7e8b
Minh An Dao <minhan7497@gmail.com>
2019-10-07 09:46:25 +0700

    fix sox in python

--- PiAudioRecord.py
+++ PiAudioRecord.py
@@ -56,7 +56,8 @@ def start():
     #converter.convert(samplerate=target_rate)
     #converter.build(org_wav,des_wav)
     # equivalent to "sox -r target_rate org_wav des_wav" in command line
-    args = ['-r', str(target_rate), org_wav, des_wav]
+    # equivalent to "sox org_wav des_wav rate target_rate" in command line
+    args = [org_wav, des_wav,'rate', str(target_rate)]
     sox.core.sox(args)
 
 def save_joy(joy_now):

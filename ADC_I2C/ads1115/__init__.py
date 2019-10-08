59ab56a814763ef0ce113a9733f494a6dddc890f
Minh An Dao <minhan7497@gmail.com>
2019-10-08 11:01:14 +0700

    add ADC

---
+++ ADC_I2C/ads1115/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+ADS1115 Reader module
+Copyright (c) 2019 Minh-An Dao for Maker Innovation Space, Can Tho University
+Copyright (c) 2017 Carter Nelson for Adafruit Industries
+All rights reserved.
+
+"""
+
+__version__ = '1.0'

817a7b5ca31f3665bfc6f62c29decb46cabe0613
Minh An Dao <minhan7497@gmail.com>
2019-10-10 14:11:58 +0700

    add python popen for c code

---
+++ 595/main.py
@@ -0,0 +1,22 @@
+"""------------------------------------------------------------*-
+  Peripheral controller for Raspberry Pi
+  Manipulate 6 IC 74HC595 using C and Subprocess module
+  Tested on: Raspberry Pi 3 B+
+  (c) Minh-An Dao 2019
+  (C) Yasuhiro Ogino 2019
+  version 1.00 - 10/10/2019
+ --------------------------------------------------------------
+ *
+ *
+ --------------------------------------------------------------"""
+import subprocess as subpro
+import time
+
+pi_dir = "./peripheral_init"
+pm_dir = "./peripheral_main " + "12" + " " + "34" + " " + "56" + " " + "12" + " " + "34" + " " + "56"
+
+while True:
+    subpro.Popen([pi_dir], shell=True)
+    time.sleep(3)
+    subpro.Popen([pm_dir], shell=True)
+    time.sleep(3)
